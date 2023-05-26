fe4b6b0f4d9719b02cfbfca4a0c268719223cb77
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-05-26 10:51:04 +0545

    Add: Main program file 'app.py'

---
+++ Flask/app.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import CNN
+from PIL import Image
+import torch
+import numpy as np
+import torchvision.transforms.functional as TF
+
+disease_info = pd.read_csv('disease_info.csv' , encoding='cp1252')
+
+model = CNN.CNN(39)    
+model.load_state_dict(torch.load("plant_disease_model_1_latest.pt"))
+model.eval()
+
+def prediction(image_path):
+    image = Image.open(image_path)
+    image = image.resize((224, 224))
+    input_data = TF.to_tensor(image)
+    input_data = input_data.view((-1, 3, 224, 224))
+    output = model(input_data)
+    output = output.detach().numpy()
+    index = np.argmax(output)
+    return index
