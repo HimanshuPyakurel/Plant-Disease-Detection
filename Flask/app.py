3d7cb97119c77abe29c407ac27874353cc8e0436
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-05-30 20:23:13 +0545

    Add: Static Folder

--- Flask/app.py
+++ Flask/app.py
@@ -1,15 +1,18 @@
-import pandas as pd
-import CNN
+import os
+from flask import Flask, redirect, render_template, request,url_for, session
 from PIL import Image
-import torch
-import numpy as np
 import torchvision.transforms.functional as TF
+import CNN
+import numpy as np
+import torch
+import pandas as pd
 from flask_mysqldb import MySQL
 import MySQLdb.cursors
-from flask import Flask, redirect, render_template, request,url_for, session
 import re
 
+
 disease_info = pd.read_csv('disease_info.csv' , encoding='cp1252')
+supplement_info = pd.read_csv('supplement_info.csv',encoding='cp1252')
 
 model = CNN.CNN(39)    
 model.load_state_dict(torch.load("plant_disease_model_1_latest.pt"))
@@ -24,6 +27,7 @@ def prediction(image_path):
     output = output.detach().numpy()
     index = np.argmax(output)
     return index
+
 
 app = Flask(__name__)
 
@@ -103,6 +107,30 @@ def home_page():
 def ai_engine_page():
     return render_template('index.html')
 
+
+@app.route('/submit', methods=['GET', 'POST'])
+def submit():
+    if request.method == 'POST':
+        image = request.files['image']
+        filename = image.filename
+        file_path = os.path.join('static/uploads', filename)
+        image.save(file_path)
+        print(file_path)
+        pred = prediction(file_path)
+        title = disease_info['disease_name'][pred]
+        description =disease_info['description'][pred]
+        prevent = disease_info['Possible Steps'][pred]
+        image_url = disease_info['image_url'][pred]
+        supplement_name = supplement_info['supplement name'][pred]
+        supplement_image_url = supplement_info['supplement image'][pred]
+        supplement_buy_link = supplement_info['buy link'][pred]
+        return render_template('submit.html' , title = title , desc = description , prevent = prevent , 
+                               image_url = image_url , pred = pred ,sname = supplement_name , simage = supplement_image_url , buy_link = supplement_buy_link)
+
+@app.route('/market', methods=['GET', 'POST'])
+def market():
+    return render_template('market.html', supplement_image = list(supplement_info['supplement image']),
+                           supplement_name = list(supplement_info['supplement name']), disease = list(disease_info['disease_name']), buy = list(supplement_info['buy link']))
 
 if __name__ == '__main__':
     app.run(port=8000, debug=True)
