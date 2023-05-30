97958fc64b040a454bf380c2f3a9ac9fe95f37b4
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-05-30 20:10:09 +0545

    Fix: app.py

--- Flask/app.py
+++ Flask/app.py
@@ -4,6 +4,10 @@ from PIL import Image
 import torch
 import numpy as np
 import torchvision.transforms.functional as TF
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+from flask import Flask, redirect, render_template, request,url_for, session
+import re
 
 disease_info = pd.read_csv('disease_info.csv' , encoding='cp1252')
 
@@ -20,3 +24,85 @@ def prediction(image_path):
     output = output.detach().numpy()
     index = np.argmax(output)
     return index
+
+app = Flask(__name__)
+
+app.secret_key = 'xyzsdfg'
+  
+# Create a MySql Database and your details must modify the below:- 
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'Hp521678' #Database password
+app.config['MYSQL_DB'] = 'user' #Database_name
+  
+mysql = MySQL(app)
+
+@app.route('/')
+
+@app.route('/login', methods =['GET', 'POST'])
+def login():
+    mesage = ''
+    if request.method == 'POST' and 'email' in request.form and 'password' in request.form:
+        email = request.form['email']
+        password = request.form['password']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM user WHERE email = % s AND password = % s', (email, password, ))
+        user = cursor.fetchone()
+        if user:
+            session['loggedin'] = True
+            session['userid'] = user['userid']
+            session['name'] = user['name']
+            session['email'] = user['email']
+            mesage = 'Logged in successfully !'
+            return redirect(url_for('home_page'))
+        else:
+            mesage = 'Please enter correct email / password !'
+    return render_template('login.html', mesage = mesage)
+  
+@app.route('/logout')
+def logout():
+    session.pop('loggedin', None)
+    session.pop('userid', None)
+    session.pop('email', None)
+    return redirect(url_for('login'))
+  
+@app.route('/register', methods =['GET', 'POST'])
+def register():
+    mesage = ''
+    if request.method == 'POST' and 'name' in request.form and 'password' in request.form and 'email' in request.form :
+        userName = request.form['name']
+        password = request.form['password']
+        email = request.form['email']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM user WHERE email = % s', (email, ))
+        account = cursor.fetchone()
+        if account:
+            mesage = 'Account already exists !'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            mesage = 'Invalid email address !'
+        elif not len(password) >= 4:
+            mesage = 'Password must be at least 4 character'
+        elif re.search('[a-zA-Z]',password) is None: 
+            mesage = "Make sure your password has a letter in it"
+        elif not userName or not password or not email:
+            mesage = 'Please fill out the form !'
+        else:
+            cursor.execute('INSERT INTO user VALUES (Null,% s, % s, % s)', (userName, email, password))
+            mysql.connection.commit()
+            mesage = 'You have successfully registered !'
+    elif request.method == 'POST':
+        mesage = 'Please fill out the form !'
+    return render_template('register.html', mesage = mesage)
+
+@app.route('/home')
+def home_page():
+    return render_template('home.html')
+
+
+@app.route('/index')
+def ai_engine_page():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(port=8000, debug=True)
